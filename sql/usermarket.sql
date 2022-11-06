37a90d0daccc630257cd461ac9728d35fbb664f4
Sabbir Mahmud <55854329+sabbirsp38@users.noreply.github.com>
2022-11-06 17:29:46 +0600

    database
    database

--- sql/usermarket.sql
+++ sql/usermarket.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2022 at 04:55 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Nov 06, 2022 at 12:28 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -50,13 +50,6 @@ CREATE TABLE `adds` (
   `uni_id` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `adds`
---
-
-INSERT INTO `adds` (`id`, `P_title`, `P_catagory`, `p_sub_catagory`, `p_decription`, `P_city`, `p_sate`, `p_price`, `p_condition`, `P_item_type`, `p_contact_number`, `P_imag1`, `P_imag2`, `P_imag3`, `P_imag4`, `time`, `city`, `county`, `zip_code`, `uni_id`) VALUES
-(8, 'Ad Title', 0, 0, 'Ad Description ', '', '', '452452452', 'Condition', 'Item Type', '9849849', '2ca8c07ce5.png', '2ca8c07ce56d7ed21f359c22d71ca52e.jpg', '2ca8c07ce56d7ed21f359c22d71ca5.png', '2ca8c07ce56d7ed21f35.png', '2022-11-04 04:49:18', 'Dhaka', 'Bangladesh', '1620', '64b4c5bed2cc943f639c0e11dbfff14a');
-
 -- --------------------------------------------------------
 
 --
@@ -66,9 +59,16 @@ INSERT INTO `adds` (`id`, `P_title`, `P_catagory`, `p_sub_catagory`, `p_decripti
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `name` varchar(250) NOT NULL,
-  `user_name` varchar(250) NOT NULL,
+  `username` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Sabbir', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,7 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
-  `cat_id` int(1) NOT NULL,
+  `cat_id` int(11) NOT NULL,
   `en_title` varchar(250) NOT NULL,
   `de_title` varchar(250) NOT NULL,
   `icon` varchar(250) NOT NULL,
@@ -88,17 +88,18 @@ CREATE TABLE `category` (
   `de_banner_dec` varchar(250) NOT NULL,
   `en_botton_text` varchar(250) NOT NULL,
   `de_botton_text` varchar(250) NOT NULL,
-  `button_url` varchar(250) NOT NULL
+  `button_url` varchar(250) NOT NULL,
+  `banner_img` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`id`, `cat_id`, `en_title`, `de_title`, `icon`, `en_banner_title`, `de_banner_title`, `en_banner_dec`, `de_banner_dec`, `en_botton_text`, `de_botton_text`, `button_url`) VALUES
-(1, 0, 'name English 1', 'Name Garman 1', 'cat1.png', '', '', '', '', '', '', ''),
-(2, 0, 'name English 2', 'Name Garman 2', 'cat2.png', '', '', '', '', '', '', ''),
-(3, 3, 'Genarel', 'Garman Genarel', 'cat1.png', 'English Title ', 'Garman Title ', 'English Description ', 'Garman Description ', 'English button', 'Garman button', 'profile/post-ad.php');
+INSERT INTO `category` (`id`, `cat_id`, `en_title`, `de_title`, `icon`, `en_banner_title`, `de_banner_title`, `en_banner_dec`, `de_banner_dec`, `en_botton_text`, `de_botton_text`, `button_url`, `banner_img`) VALUES
+(1, 1, 'name English 1', 'Name Garman 1', 'cat1.png', '', '', '', '', '', '', '', ''),
+(3, 3, 'Genarel ', 'Garman Genarel', 'cat1.png', 'English Title database', 'Garman Title database', 'English Description database', 'Garman Description database', 'English button database', 'Garman button database', 'profile/post-ad.php', ''),
+(7, 0, 't', 't', 'dbbf1baad3.png', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -137,6 +138,15 @@ CREATE TABLE `sub_catagory` (
   `de_name` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `sub_catagory`
+--
+
+INSERT INTO `sub_catagory` (`id`, `parent_cat`, `en_name`, `de_name`) VALUES
+(1, 3, 'English Sub Cat', 'Garman Sub Cat'),
+(2, 3, 'English Sub Cat 2', 'Garman Sub Cat 2'),
+(3, 3, 'English Sub Cat 2', 'Garman Sub Cat 3');
+
 -- --------------------------------------------------------
 
 --
@@ -160,8 +170,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `password`, `uni_id`, `email`, `time`, `city`, `county`, `zip_code`) VALUES
-(6, 'sabbir', 'a', '64b4c5bed2cc943f639c0e11dbfff14a', 'a@a.a', '2022-11-03 05:52:57', 'Dhaka', 'Bangladesh', '1620'),
-(7, 'Subject', 'b', 'ddf3e60b0215d15bf4e0c14e049cc454', 'b@b.b', '2022-11-04 11:17:24', 'Dhaka', 'india', '111');
+(6, 'sabbir', 'a', '64b4c5bed2cc943f639c0e11dbfff14a', 'a@a.a', '2022-11-03 05:52:57', 'Dhaka', 'Bangladesh', '1620');
 
 --
 -- Indexes for dumped tables
@@ -217,25 +226,25 @@ ALTER TABLE `adds`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `sub_catagory`
 --
 ALTER TABLE `sub_catagory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
